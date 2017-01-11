88348225beaf367ec50511e2f2a999d0e71095b4
Jake Burga <jrburga@mit.edu>
2017-01-11 14:07:40 -0500

    core and tools

--- vgdl/core.js
+++ vgdl/core.js
@@ -1,12 +1,422 @@
-gamejs = require('gamejs');
+var Core = function () {
+	var that = Object.create(Core.prototype);
 
-var VGDLParser = function () {
-	var that = Object.create(VGDLParser.prototype);
+	var gamejs = require('gamejs');
+	var tools = require('/tools.js');
+	var ontology = require('/ontology.js')
 
-	var verbose = false;
 
-	that.playGame = function (game_str, map_str) {
-		var game = VGDLParser().parseGame(game_str);
+
+	var disableContinuousKeyPress = true;
+	var actionToKeyPress = {};
+	actionToKeyPress[(-1, 0)] = gamejs.event.K_LEFT;
+	actionToKeyPress[(1, 0)] = gamejs.event.K_RIGHT;
+	actionToKeyPress[(0, 1)] = gamejs.event.K_DOWN;
+	actionToKeyPress[(0, -1)] = gamejs.event.K_UP;
+
+	var keyPresses = {273: 'up', 274: 'down', 276: 'left', 275: 'right', 32: 'spacebar'};
+	var emptyKeyState = new Array(323).fill(0);
+	var colorDict = {'[0, 200, 0]': 'GREEN',
+									 '[0, 0, 200]': 'BLUE',
+									 '[200, 0, 0]': 'RED',
+									 '[90, 90, 90]': 'GRAY',
+									 '[250, 250, 250]': 'WHITE',
+									 '[0, 0, 0]': 'BLACK',
+									 '[250, 160, 0]': 'ORANGE',
+									 '[250, 250, 0]': 'YELLOW',
+									 '[250, 200, 200]': 'PINK',
+									 '[250, 212, 0]': 'GOLD',
+									 '[250, 50, 50]': 'LIGHTRED',
+									 '[250, 200, 100]': 'LIGHTORANGE',
+									 '[50, 100, 250]': 'LIGHTBLUE',
+									 '[50, 250, 50]': 'LIGHTGREEN',
+									 '[150, 150, 150]': 'LIGHTGRAY',
+									 '[30, 30, 30]': 'DARKGRAY',
+									 '[20, 20, 100]': 'DARKBLUE',
+									 '[140, 20, 140]': 'PURPLE'};
+
+	that.VGDLParser = function () {
+		var that = Object.create(VGDLParser.prototype);
+		var verbose = false;
+
+		/**
+		 * @param  {[type]}
+		 * @return {[type]}
+		 */
+		that.parseGame = function (tree) {
+
+			if (!tree instanceof tools.Node())
+				tree = tools.indentTreeParser(tree).children[0];
+
+			[sclass, args] = that._parseArgs(tree.content);
+			that.game = sclass(args)
+			tree.children.forEach(function (child) {
+				parse[child.content](child.children);
+			});
+
+			return that.game;
+		}
+
+		var _eval(estr) {
+			return eval(estr);
+		}
+
+		var parse = {
+			// parseInteractions
+			'InteractionSet' : function (inodes) {
+				inodes.forEach(function (inode) {
+					if (inode.content.indexOf('>') != -1) {
+						var [pair, edef] = inode.content.split('>').map(function (s) {
+							return s.trim();
+						});
+						var [eclass, args] = that._parseArgs(edef);
+						that.game.collision_eff.push(
+							pair.split(' ').map(function (s) {
+								return s.trim();
+							}).concat([eclass, args])
+						);
+						if (verbose) {
+							console.log('Collision', pair, 'has effect:', edef);
+						}
+					}
+				});
+
+			},
+			//parseSprites
+			'SpriteSet' : function (snodes, parentClass = null, parentargs = {}, parenttypes = []) {
+				snodes.forEach(function (snode) {
+					console.assert(snode.content.indexOf('>') != -1);
+					var [key, sdef] = snode.content.split('>').map(function (s) {
+						return s.trim();
+					});
+					var [sclass, args] = that._parseArgs(sdef, parentclass, parentargs.copy());
+					var stypes = parenttypes.concat(keys);
+
+					if (args.indexOf('singleton') != -1) {
+						if (args['singleton'] == true) 
+							that.game.singletons.push(key);
+						args = tools.clone(args);
+						delete args['singleton'];
+					}
+
+					if (snode.children.length == 0) {
+						if (verbose) 
+							console.log('Defining:', key, sclass, args, stypes);
+						that.game.sprite_constr[key] = [sclass, args, stypes];
+						if (that.game.sprite_order.indexOf(key) != -1) {
+							var index = that.game.sprite_order.indexOf(key);
+							that.game.sprite_order.splice(index, 1);
+						}
+						that.game.sprite_order.push(key);
+					} else {
+						parse['SpriteSet'](snode.children, sclass, args, stypes);
+					}
+				})
+			},
+			//parseTerminations
+			'TerminationSet' : function (tnodes) {
+				tnodes.forEach(function (tnode) {
+					var [sclass, args] = self._parsArgs(tnode.content);
+					if (verbose)
+						console.log('Adding:', sclass, args);
+					that.game.terminations.append(sclass(args));
+				});
+			},
+			//parseConditions
+			'ConditionalSet' : function (cnodes) {
+				cnodes.forEach(function (cnode) {
+					if (cnode.content.indexOf('>') != -1) {
+						var [conditional, interaction] = cnode.content.split('>').map(function (s) {
+							return s.trim();
+						});
+
+						var [cclass, cargs] = that._parseArgs(conditional);
+						var [eclass, eargs] = that._parseArgs(interaction);
+
+						that.game.conditions.push([cclass(cargs), [eclass, eargs]]);
+					}
+				});
+
+			}
+		}
+
+
+		var parseMappings = function (mnodes) {
+			mnodes.forEach(function (mnode) {
+				var [c, val] = mnode.split('>').map(function (x) {
+					return x.trim();
+				});
+
+				console.assert(c.length == 1, "Only single character mappings allowed");
+
+				var keys = val.split(' ').map(function (x) {
+					return x.trim();
+				});
+
+				if (verbose) 
+					console.log("Mapping", c, keys);
+
+				that.game.char_mapping[c] = keys;
+			});
+
+		}
+
+		var _parseArgs = function (string, sclass=null, args=null) {
+			if (args === null)
+				args = {};
+
+			var sparts = string.split(' ').map(function (s) {
+				return s.trim();
+			});
+
+			var sclass;
+			if (sparts.length == 0)
+				return [sclass, args];	
+
+			if (sparts[0].indexOf('=') == -1) {
+				sclass = _eval(sparts[0]);
+				sparts = sparts.slice(1);
+			}
+
+			sparts.forEach(function (spart) {
+				var [k, val] = spart.split('=');
+
+				try {
+					args[k] = _eval(val);
+				} catch  (e) {
+					args[k] = val;
+				}
+			});
+
+			return [sclass, args];
+		}
+
+		Object.freeze(that);
+		return that;
+	}
+
+	/**
+	 * params {**kwargs} any number of arguments which will be used in the game
+	 */
+	that.BasicGame = function () {
+		var that = Object.create(BasicGame.prototype);
+
+		var Immovable = ontology.Immovable;
+		var DARKGRAY = ontology.DARKGRAY;
+		var MovingAvatar = ontology.MovingAvatar;
+		var GOLD = ontology.GOLD;
+
+		var MAX_SPRITES = 10000;
+
+		var default_mapping = {'w': ['wall'], 'A', ['avatar']};
+
+		var block_size = 10;
+		var frame_rate = 20;
+		var load_save_enabled;
+
+		//INIT
+
+		//grab all arguments
+
+		that.sprite_constr = {'wall': [Immovable, {'color': DARKGRAY}, ['wall']],
+													'avatar': [MovingAvatar, {}, ['avatar']]};
+
+		that.sprite_order = ['wall', 'avatar'];
+
+		that.singletons = [];
+
+		that.collision_eff = [];
+
+		that.char_mapping = {};
+
+		that.terminations = [Termination()];
+
+		that.conditions = [];
+
+		that.resources_limits;
+		that.resources_colors;
+
+		that.is_stochastic = false;
+		that._lastsaved = null;
+		that.win = null;
+		that.effectList = [];
+		that.reset();
+
+		that.reset = function () {
+			that.score = 0;
+			that.time = 0;
+			that.ended = false;
+			that.num_sprites = 0;
+			that.kill_list = [];
+			that.all_killed = [];
+
+		}
+
+		that.buildLevel = function (lstr) {
+			var stochastic_effects = ontology.stochastic_effects;
+
+			var lines = lstr.split('\n').filter(function (l) {return l.length > 0});
+			var lengths = lines.map(function (line) {return line.length});
+
+			console.assert(Math.min(lengths) == Math.min(lengths), "Inconsistent line lengths");
+
+			that.width = lengths[0];
+			that.height = lines.length;
+
+			console.assert(that.width > 1 and that.height > 1, 'Level too small');
+
+			that.block_size = Math.max(2, Math.floor(800/Math.max(that.width, that.height)));
+			that.screensize = [that.width*that.block_size, that.height*that.block_size];
+
+			that.sprite_constr.forEach(function (item) {
+				var [res_time, [sclass, args, _]] = item;
+				if (sclass.prototype instanceof core.Resource) {
+					if (args['res_type']) 
+						res_type = args['res_type'];
+					if (args['color'])
+						that.resources_colors[res_type] = args['color'];
+					if (args['limit'])
+						that.resources_limits[res_type] = args['limit'];
+				}
+			});
+
+			lines.forEach(function (line, row) {
+				line.forEach(function (c, col) {
+					if (that.char_mapping.indexOf(c) != -1) {
+						var pos = [col*that.block_size, row*that.block_size];
+						that._createSprite(that.char_mapping[c], pos);
+					} else if (that.default_mapping.indexOf(c) != -1) {
+						var pos = [col*that.block_size, row*that.block_size];
+						that._createSprite(that.default_mapping[c], pos);
+					}
+				})
+			});
+
+			that.kill_list = [];
+
+			that.collision_eff.forEach(function (item) {
+				var [_, _, effect, _] = item;
+				if (stochastic_effects.indexOf(effect) != -1)
+					that.is_stochastic = true;
+			})	
+		}
+
+		that.emptyBlocks = function () {
+			var alls = //iterate over all the sprites
+		}
+
+		that.randomizeAvatar = function () {
+			if (that.getAvatars().length == 0)
+				that._createSprite(['avatar'], )
+		}
+
+		that._createSprite = function (keys, pos) {
+			var res = [];
+			keys.forEach(function (key) {
+				if (that.num_sprites > MAX_SPRITES) {
+					console.log('Sprite limit reached.');
+					return;
+				}
+				var [sclass, args, stypes] = that.sprite_constr[key];
+
+				var anyother = false;
+				stypes.reverse().forEach(function (pk) {
+					if (that.singletons.indexOf(pk) == -1){
+						if (that.numSprites(pk) > 0) {
+							anyother = true;
+							break;
+						}
+					}
+				});
+
+				var s = sclass(pos, [that.block_size, that.block_size], key, args);
+				s.stypes = stypes;
+				that.sprite_groups[key].append(s);
+				that.num_sprites += 1;
+				if (s.is_stochastic)
+					that.is_stochastic = true;
+				res.push(s);
+			});
+			return res;
+
+		}
+
+		that._createSprite_cheap = function (key, pos) {
+
+		}
+
+		that._initScreen = function (size) {
+			that.screen = gamejs.display.setMode(size);
+			
+		}
+
+		that.__iter__ = function () {
+
+		}
+
+		that.numSprites = function (key) {
+
+		}
+
+		that.getSprites = function (key) {
+
+		}
+
+		that.getAvatars = function () {
+
+		}
+
+		that.getObjects = function () {
+
+		}
+
+		that.getFullState = function (as_string = false) {
+
+		}
+
+		that.setFullState = function (fs, as_string = false) {
+
+		}
+
+		that.getFullStateColorized = function (as_string = false) {
+
+		}
+
+		that._clearAll = function (onscreen = true) {
+
+		}
+
+		that._drawAll = function () {
+
+		}
+
+		that._updateCollisionDict = function (changedsprite) {
+
+		}
+
+		that._eventHandling = function () {
+
+		}
+
+		that.startGame = function () {
+			that._initScreen();
+		}
+
+		that.getPossibleActions = function () {
+
+		}
+
+		that.tick = function (action) {
+
+		}
+
+		Object.freeze(that);
+		return that;
+	}
+
+	// Static method of VGDLParser
+	that.VGDLParser.prototype.playGame = function (game_str, map_str) {
+		var game = that.VGDLParser().parseGame(game_str);
 
 		game.buildLevel(map_str);
 		// game.uiud;
@@ -15,261 +425,102 @@ var VGDLParser = function () {
 		return game;
 	}
 
-	that.parseGame = function (tree) {
+	that.Avatar = function () {
+		var that = Object.create(Avatar.prototype);
 
-		return;
+		that.shrinkfactor = 0.15;
+		that.actions = that.declare_possible_actions();
+
+		Object.freeze(that);
+		return that;
 	}
 
-	that.parseIntersections = function (inodes) {
 
+	that.VGDLSprite = function () {
+		var that = Object.create(VGDLSprite.prototype);
+
+		that.name = null;
+		that.COLOR_DISC = [20, 80, 140, 200];
+		that.dirtyrects = [];
+		that.is_static = false;
+		that.only_active = false;
+		that.is_avatar = false;
+		that.is_stochastic =false;
+		that.color = null;
+		that.cooldown = 0;
+		that.speed = null;
+		that.mass = 1;
+		that.physicstype = null;
+		that.shrinkfactor = 0;
+
+		// INIT
+
+		that.update = function (game) {
+
+		}
+
+		that._updatePos = function (orientation, speed=null) {
+
+		}
+
+		that._velocity = function () {
+
+		}
+
+		that.lastdirection = function () {
+
+		}
+
+		that._draw = function (game) {
+
+		}
+
+		that._drawResources = function (game, screen, rect) {
+
+		}
+
+		that._clear = function (screen, background, double=null) {
+
+		}
+
+		Object.freeze(that);
+		return that;
 	}
 
-	that.parseTerminations = function (tnodes) {
+	that.Resource = function () {
+		var that = Object.create(Resource.prototype);
 
+		that.value = 1;
+		that.limit = 2;
+		that.res_type = null;
+
+		that.resourceType = function () {
+
+		}
+
+		Object.freeze(that);
+		return that;
 	}
 
-	that.parseConditions = function (cnodes) {
 
+	that.Termination = function () {
+		var that = Object.create(Termination.prototype);
+
+		that.isDone = function (game) {
+
+		}
+
+		Object.freeze(that);
+		return that;
 	}
 
-	that.parseSprites = function (snodes, parentClass = None, parentargs = {}, parenttypes = []) {
+	that.Conditional = function () {
+		var that = Object.create(Conditional.prototype);
 
+		Object.freeze(that);
+		return that;
 	}
 
-	that.parseMappings = function (mnodes) {
-
-	}
-
-	that._parseArgs = function (s, sclass=None, args=None) {
-
-	}
-	Object.freeze(that);
-	return that;
-}
-
-var BasicGame = function () {
-	var that = Object.create(BasicGame.prototype);
-
-	var MAX_SPRITES = 10000;
-
-	var default_mapping = {'w': ['wall'], 'A', ['avatar']};
-
-	var block_size = 10;
-	var frame_rate = 20;
-	var load_save_enabled;
-
-	//INIT
-
-	//grab all arguments
-
-	that.sprite_constr = {};
-
-	that.sprite_order = ['wall', 'avatar'];
-
-	that.singletons = [];
-
-	that.collision_eff = [];
-
-	that.char_mapping = {};
-
-	that.terminations = [Termination()];
-
-	that.conditions = [];
-
-	that.resources_limits;
-	that.resources_colors;
-
-	that.is_stochastic = false;
-	that._lastsaved = null;
-	that.win = null;
-	that.effectList = [];
-	that.reset();
-
-	that.reset = function () {
-
-	}
-
-	that.buildLevel = function (lstr) {
-
-	}
-
-	that.emptyBlocks = function () {
-
-	}
-
-	that.randomizeAvatar = function () {
-
-	}
-
-	that._createSprite = function (keys, pos) {
-
-	}
-
-	that._createSprite_cheap = function (key, pos) {
-
-	}
-
-	that._initScreen = function (size) {
-		that.screen = gamejs.display.setMode(size);
-		
-	}
-
-	that.__iter__ = function () {
-
-	}
-
-	that.numSprites = function (key) {
-
-	}
-
-	that.getSprites = function (key) {
-
-	}
-
-	that.getAvatars = function () {
-
-	}
-
-	that.getObjects = function () {
-
-	}
-
-	that.getFullState = function (as_string = false) {
-
-	}
-
-	that.setFullState = function (fs, as_string = false) {
-
-	}
-
-	that.getFullStateColorized = function (as_string = false) {
-
-	}
-
-	that._clearAll = function (onscreen = true) {
-
-	}
-
-	that._drawAll = function () {
-
-	}
-
-	that._updateCollisionDict = function (changedsprite) {
-
-	}
-
-	that._eventHandling = function () {
-
-	}
-
-	that.startGame = function () {
-		that._initScreen();
-	}
-
-	that.getPossibleActions = function () {
-
-	}
-
-	that.tick = function (action) {
-
-	}
-	
-	Object.freeze(that);
-	return that;
-}
-
-var Avatar = function () {
-	var that = Object.create(Avatar.prototype);
-
-	that.shrinkfactor = 0.15;
-	that.actions = that.declare_possible_actions();
-
-	Object.freeze(that);
-	return that;
-}
-
-
-var VGDLSprite = function () {
-	var that = Object.create(VGDLSprite.prototype);
-
-	that.name = null;
-	that.COLOR_DISC = [20, 80, 140, 200];
-	that.dirtyrects = [];
-	that.is_static = false;
-	that.only_active = false;
-	that.is_avatar = false;
-	that.is_stochastic =false;
-	that.color = null;
-	that.cooldown = 0;
-	that.speed = null;
-	that.mass = 1;
-	that.physicstype = null;
-	that.shrinkfactor = 0;
-
-	// INIT
-
-	that.update = function (game) {
-
-	}
-
-	that._updatePos = function (orientation, speed=null) {
-
-	}
-
-	that._velocity = function () {
-
-	}
-
-	that.lastdirection = function () {
-
-	}
-
-	that._draw = function (game) {
-
-	}
-
-	that._drawResources = function (game, screen, rect) {
-
-	}
-
-	that._clear = function (screen, background, double=null) {
-
-	}
-
-	Object.freeze(that);
-	return that;
-}
-
-var Resource = function () {
-	var that = Object.create(Resource.prototype);
-
-	that.value = 1;
-	that.limit = 2;
-	that.res_type = null;
-
-	that.resourceType = function () {
-
-	}
-
-	Object.freeze(that);
-	return that;
-}
-
-
-var Termination = function () {
-	var that = Object.create(Termination.prototype);
-
-	that.isDone = function (game) {
-
-	}
-
-	Object.freeze(that);
-	return that;
-}
-
-var Conditional = function () {
-	var that = Object.create(Conditional.prototype);
-
-	Object.freeze(that);
-	return that;
+Object.freeze(that);
+return that;
 }
--- vgdl/tools.js
+++ vgdl/tools.js
@@ -1,40 +1,156 @@
-// var gamejs = require('gamejs');
+/**
+ * @param  {String}	tabSize
+ * @return {String}	new string with expanded tabs
+ * pulled from
+ * http://cwestblog.com/2012/01/23/javascript-string-prototype-expandtabs-revisited/
+ * to mimic Python's String.expandtab() function
+ */
+String.prototype.expandTabs = function(tabSize) {
+  var spaces = (new Array((tabSize = tabSize || 8 ) + 1)).join(" ");
+  return this.replace(/([^\r\n\t]*)\t/g, function(a, b) {
+    return b + spaces.slice(b.length % tabSize);
+  });
+};
 
-var vectNorm = function (vector) {
-
-}
-
-var unitVector = function (vector) {
-
-}
-
-var oncePerStep = function (sprite, game, name) {
-
-}
-
-var triPoints = function (rect, orientation) {
-
-}
-
-var roundedPoints = function (rect) {
-
-}
-
-var squarePoints = function (center, size) {
-
-}
-
-var Node = function () {
+/**
+ * Tools used for vgdl. Some of these functions alraedy exist in the gamejs library
+ */
+var Tools = function () {
 	var that = Object.create(Node.prototype);
+
+	/**
+	 * @param  {[type]}
+	 * @return {[type]}
+	 */
+	that.clone = function (obj) {
+	    if (null == obj || "object" != typeof obj) return obj;
+	    var copy = obj.constructor();
+	    for (var attr in obj) {
+	        if (obj.hasOwnProperty(attr)) copy[attr] = obj[attr];
+	    }
+	    return copy;
+	}
+
+
+	/**
+	 * @param  {[type]}
+	 * @return {[type]}
+	 */
+	that.vectNorm = function (vector) {
+		return Math.sqrt(Math.pow(vector[0], 2) + Math.pow(vector[1], 2));
+	}
+
+	/**
+	 * @param  {[type]}
+	 * @return {[type]}
+	 */
+	that.unitVector = function (vector) {
+		var norm = that.vectNorm(vector);
+		if (norm > 0)
+			return [vector[0]/norm, vector[1]/norm];
+		else
+			return [1, 0];	
+	}
+
+	/**
+	 * @param  {[type]}
+	 * @param  {[type]}
+	 * @param  {[type]}
+	 * @return {[type]}
+	 */
+	that.oncePerStep = function (sprite, game, name) {
+		var name = '_'+name;
+
+		if (sprite[name]) 
+			if (sprite[name] == game.time) return false;
+		
+		sprite[name] = game.time;
+		return true;
+	}
+
+	/**
+	 * @param  {[type]}
+	 * @param  {[type]}
+	 * @return {[type]}
+	 */
+	that.triPoints = function (rect, orientation) {
+		var p1 = [rect.center[0] + orientation[0]*rect.w/3,
+				  rect.center[1] + orientation[1]*rect.h/3];
+		var p2 = [rect.center[0] + orientation[0]*rect.w/4,
+			      rect.center[1] + orientation[1]*rect.h/4];
+		var orthdir = [orientation[1], -orientation[0]];
+		var p2a = [p2[0]-orthdir[0]*rect.w/6,
+				   p2[1]-orthdir[1]*rect.h/6];
+		var p2b = [p2[0]+orthdir[0]*rect.w/6,
+				   p2[1]+orthdir[1]*rect.h/6];
+		return [p1, p2a, p2b].map(function (p) {return [p[0], p[1]]});
+	}
+
+	/**
+	 * @param  {[type]}
+	 * @return {[type]}
+	 */
+	that.roundedPoints = function (rect) {
+
+	}
+
+	/**
+	 * @param  {[type]}
+	 * @param  {[type]}
+	 * @return {[type]}
+	 */
+	that.squarePoints = function (center, size) {
+
+	}
+
+	/**
+	 * @param {[type]}
+	 * @param {[type]}
+	 * @param {[type]}
+	 */
+	that.Node = function (content, indent, parent=null) {
+		var that = Object.create(Node.prototype);
+		that.children = [];
+		that.content = content;
+		that.indent = indent;
+		if (parent) 
+			parent.insert(that);
+		else
+			that.parent = null;
+
+		/**
+		 * @param  {[type]}
+		 * @return {[type]}
+		 */
+		that.insert = function (node) {
+			if (that.indent < node.indent) {
+				if (that.children.length > 0)
+					console.assert(that.children[0].indent == node.indent, 'children indentations must match');
+				that.children.append(node);
+				node.parent = that;
+			} else {
+				console.assert(that.parent, 'Root node too indented?');
+				that.parent.insert(node);
+			}
+		}
+
+		Object.freeze(that);
+		return that;
+	}
+
+	/**
+	 * @param  {[type]}
+	 * @param  {Number}
+	 * @return {[type]}
+	 */
+	that.indentTreeParser = function (s, tabsize=8) {
+		var s = s.expandTabs(tabsize);
+	}
+
+	that.listRotate = function (list, n) {
+		return list.slice(n).concat(list.slice(0, n));
+	}
 
 	Object.freeze(that);
 	return that;
-}
-
-var indentTreeParser = function (s, tabsize=8) {
-
-}
-
-var listRotate = function (list, n) {
-
 }
