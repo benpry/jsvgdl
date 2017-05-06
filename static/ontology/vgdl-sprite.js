3a9c88fe4a77fde51c2aeb9604e87cac00beb8db
jrburga <jrburga@mit.edu>
2017-05-06 14:14:29 -0400

    accidentally pushed merge conflict. fixed now?

--- static/ontology/vgdl-sprite.js
+++ static/ontology/vgdl-sprite.js
@@ -109,17 +109,11 @@ VGDLSprite.prototype = {
 			this.gamejs.graphics.rect(game.screen, this.color, this.rect);
 		}
 
-<<<<<<< HEAD
 		if (Object.keys(this.resources).length)  {
 			// console.log(this.resources)
 			this._drawResources(game, game.screen, this.rect);
 		}
 
-=======
-		if (this.resources) { 
-			this._drawResources(game, game.screen, this.rect);
-		}
->>>>>>> 3ea3b015f1fcf345410f01e59017954c16e2a533
 		return;
 		// var screen = game.screen;
 		// if (this.shrinkfactor != 0)
@@ -156,11 +150,6 @@ VGDLSprite.prototype = {
 		var offset = rect.top + 2*rect.height/3;
 		var that = this;
 		Object.keys(this.resources).sort().forEach(function (r) {
-<<<<<<< HEAD
-			// console.log(game.resources_colors[r])
-=======
-			// console.log(game.resources_colors)
->>>>>>> 3ea3b015f1fcf345410f01e59017954c16e2a533
 			var wiggle = rect.width/10;
 			console.log(that.resources[r], game.resources_limits[r])
 			var prop = Math.max(0, Math.min(1, that.resources[r] / game.resources_limits[r]));
