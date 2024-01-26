b35a80522e7db8214130e65577f6ac4d4c39deac
Ben Prystawski <11021815+benpry@users.noreply.github.com>
2024-01-26 12:17:36 -0800

    updated good games list with new game names

--- mongodb/game-schema.js
+++ mongodb/game-schema.js
@@ -1,128 +1,150 @@
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 var ObjectId = mongoose.Schema.Types.ObjectId;
 
 var GameSchema = mongoose.Schema({
-	descs  : {type: [String], required: true},
-	levels : {type: [String], required: true},
-	name   : {type: String, required: true, unique : true},
-})
+	descs: { type: [String], required: true },
+	levels: { type: [String], required: true },
+	name: { type: String, required: true, unique: true },
+});
 
 GameSchema.statics.add_game = function (game_name, descs, levels, callback) {
-	var descs = descs || [''];
-	var levels = levels || [''];
-	this.create({
-		name: game_name, 
-	 	descs: descs, 
-		levels: levels
-	}, callback)
-}
+	var descs = descs || [""];
+	var levels = levels || [""];
+	this.create(
+		{
+			name: game_name,
+			descs: descs,
+			levels: levels,
+		},
+		callback,
+	);
+};
 
 GameSchema.statics.update_game = function (game_name, descs, levels, callback) {
-	this.update({name: game_name}, {
-		levels: levels,
-		descs: descs
-	}, callback)
-}
+	this.update(
+		{ name: game_name },
+		{
+			levels: levels,
+			descs: descs,
+		},
+		callback,
+	);
+};
 
 GameSchema.statics.delete_game = function (game_name, callback) {
-	this.remove({name: game_name}, (err) => {
-		if (err) {
-			console.log(err);
-		}
-	}, callback)
-}
+	this.remove(
+		{ name: game_name },
+		(err) => {
+			if (err) {
+				console.log(err);
+			}
+		},
+		callback,
+	);
+};
 
 GameSchema.statics.delete_level = function (game_name, level_num, callback) {
-	this.findOne({}, game_name, (err, game) => {
-		if (game) {
-			game.levels.splice(level_num, 1);
-			game.save();
-		} else {
-			console.log('Could not delete level. Game was not found', game_name);
-		}
-	}, callback)
-}
+	this.findOne(
+		{},
+		game_name,
+		(err, game) => {
+			if (game) {
+				game.levels.splice(level_num, 1);
+				game.save();
+			} else {
+				console.log("Could not delete level. Game was not found", game_name);
+			}
+		},
+		callback,
+	);
+};
 
-GameSchema.statics.delete_description = function (game_name, desc_num, callback) {
-	this.findOne({}, game_name, (err, game) => {
-		if (game) {
-			game.descs.splice(desc_num, 1);
-			game.save();
-		} else {
-			console.log('Could not delete description. game was not found', game_name)
-		}
-	}, callback)
-}
+GameSchema.statics.delete_description = function (
+	game_name,
+	desc_num,
+	callback,
+) {
+	this.findOne(
+		{},
+		game_name,
+		(err, game) => {
+			if (game) {
+				game.descs.splice(desc_num, 1);
+				game.save();
+			} else {
+				console.log(
+					"Could not delete description. game was not found",
+					game_name,
+				);
+			}
+		},
+		callback,
+	);
+};
 
 GameSchema.statics.get_full_games = function (callback) {
 	this.find((err, games) => {
-		callback(err, games.map(game => {
-			game.level = 0,
-			game.desc = 0
-		}))
-	})
-}
+		callback(
+			err,
+			games.map((game) => {
+				(game.level = 0), (game.desc = 0);
+			}),
+		);
+	});
+};
 
 GameSchema.statics.get_full_game = function (game_name, callback) {
-	this.findOne({name: game_name}, (err, game) => {
+	this.findOne({ name: game_name }, (err, game) => {
 		callback(err, game);
-	})
-}
+	});
+};
 
 GameSchema.statics.get_game = function (game_name, level, desc, callback) {
 	level = level || 0;
 	desc = desc || 0;
-	this.findOne({name: game_name}, (err, game) => {
+	this.findOne({ name: game_name }, (err, game) => {
 		var game_obj = {
-			name : game.name,
-			level : game.levels[level],
-			game : game.descs[desc],
-			round : 0
-		}
+			name: game.name,
+			level: game.levels[level],
+			game: game.descs[desc],
+			round: 0,
+		};
 		callback(err, game_obj);
-	})
-}
+	});
+};
 
 GameSchema.statics.get_games_list = function (callback) {
 	this.find((err, games) => {
-		var game_names = games.map(game => {
-			return game.name
-		})
+		var game_names = games.map((game) => {
+			return game.name;
+		});
 		callback(err, game_names.sort());
-	})
-}
+	});
+};
 
 var good_games = [
-	'expt_antagonist',
-	'expt_exploration_exploitation',
-	'expt_helper',
-	'expt_preconditions',
-	'expt_push_boulders',
-	'gvgai_aliens',
-	'gvgai_boulderdash',
-	'gvgai_butterflies',
-	'gvgai_chase',
-	'gvgai_frogs',
-	'gvgai_missilecommand', // new
-	'gvgai_portals',
-	'gvgai_sokoban',
-	'gvgai_zelda' // new
-]
+	"JRNL_avoidGeorge_v1",
+	"JRNL_beesAndBirds_v1",
+	"JRNL_boulderDash_v1",
+	"JRNL_plaqueAttack_v1",
+	"JRNL_portals_v1",
+	"JRNL_preconditions_v1",
+	"JRNL_pushBoulders_v1",
+	"JRNL_relational_v1",
+	"JRNL_watergame_v1",
+];
 
 GameSchema.statics.get_good_games_list = function (callback) {
 	this.find((err, games) => {
-		var game_names = games.map(game => {
-			return game.name
-		}).filter(game_name => {
-			return good_games.includes(game_name);
-		})
+		var game_names = games
+			.map((game) => {
+				return game.name;
+			})
+			.filter((game_name) => {
+				return good_games.includes(game_name);
+			});
 		callback(err, game_names.sort());
-	})
-}
+	});
+};
 
-
-
-
-module.exports = mongoose.model('Game', GameSchema);
-
-
+module.exports = mongoose.model("Game", GameSchema);
